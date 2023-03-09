9f3c5a26770098302fb6598b99fc1d54ea06a8d2
StevenSeagull1 <stevenhoekstra2005@gmail.com>
2023-03-09 10:08:59 +0100

    e

--- app/Views/mood/index.php
+++ app/Views/mood/index.php
@@ -59,41 +59,29 @@ body {
     </style>
 </head>
 <h2>uw mood</h2><canvas id="myCanvas" width="175" height="150" style="border:1px solid #d3d3d3;">
-
+Your browser does not support the HTML5 canesfsefvas tag.</canvas>
  
 <?php if (! empty($mood) && is_array($mood)): ?>
 
-    <?php for ($id = 0; $id < count($mood); $id++): ?>
+    <?php foreach ($mood as $mood_item): ?>
 
     <div class='mood'>
+        <h3><?= esc($mood_item['user']) ?></h3>
 
     <div class="main">
-        <h3> u was <?= esc($mood[$id]->mood) ?> </h3>
-        <?php 
-                if($mood[$id]->mood == NULL)
-                {
-                    if ($mood[$id - 1]->mood == NULL || $mood[$id + 1]->mood == NULL) {
-                    echo 'niet genoeg moods for gemmideld';
-                    } 
-                    else {
-                    $prev = $mood[$id - 1]->mood;
-                    $next = $mood[$id + 1]->mood;
-                    echo ($prev + $next) / 2;
-                    }
-                }
-        ?>
+        <h3> u was <?= esc($mood_item['mood']) ?> </h3>
     </div>
     
-    <h3> op <?= esc($mood[$id]->plek) ?></h3>
+    <h3> op <?= esc($mood_item['plek']) ?></h3>
 
-        <h3><?= esc($mood[$id]->datum) ?></h3>
+        <h3><?= esc($mood_item['datum']) ?></h3>
 
        
         
-        <img height="200px" width="150px" src='<?php "http://".$_SERVER['HTTP_HOST']?>/images/<?php echo ($mood[$id]->mood);?>.jpg'>
+        <img height="200px" width="150px" src='<?php "http://".$_SERVER['HTTP_HOST']?>/images/<?php echo ($mood_item['mood']);?>.jpg'>
         
     </div>
-    <?php endfor ?>
+    <?php endforeach ?>
    
 <script>
 var c = document.getElementById("myCanvas");
